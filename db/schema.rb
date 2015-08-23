cd6bc5b884d9bdfd6eaef90261124e983a25b6be
Christopher Saunders <topher6345@gmail.com>
2015-08-23 13:01:26 -0700

    Add link to posts.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823192307) do
+ActiveRecord::Schema.define(version: 20150823195547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150823192307) do
     t.datetime "updated_at",                 null: false
     t.string   "ancestry"
     t.integer  "ancestry_depth", default: 0
+    t.string   "link"
   end
 
   add_index "posts", ["ancestry"], name: "index_posts_on_ancestry", using: :btree
