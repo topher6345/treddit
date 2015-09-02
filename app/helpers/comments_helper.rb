1d7b26277641b1860edf587a8b7a02a727b8fff8
Christopher Saunders <topher6345@gmail.com>
2015-09-01 18:45:07 -0700

    Add user_highlight to CommentsHelper.

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,4 +3,13 @@ module CommentsHelper
     return "#{count} comments" if count != 1
     '1 comment'
   end
+
+  # highlight OP, regular everyone else
+  def user_highlight(post, user)
+    if post.root.user == user
+      return content_tag(:span, user.email, class: 'post-original-poster')
+    else
+      user.email
+    end
+  end
 end
