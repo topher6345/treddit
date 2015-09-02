70d056d2178bdd347904f89f6b33a3849c37ecca
Christopher Saunders <topher6345@gmail.com>
2015-09-02 10:27:08 -0700

    Remove unused variable assignment in Upvoteable.

--- app/controllers/concerns/upvoteable.rb
+++ app/controllers/concerns/upvoteable.rb
@@ -2,7 +2,7 @@ module Upvoteable
   extend ActiveSupport::Concern
   def upvote
     post = Post.find(params[:id])
-    upvote = Upvote.create! user: current_user, post: post
+    Upvote.create! user: current_user, post: post
     render json: {status: :ok }, status: :ok
   end
 
