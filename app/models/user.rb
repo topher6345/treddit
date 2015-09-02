ce381729e6a7ab9aac30fa41c2bac901ea5f2528
Christopher Saunders <topher6345@gmail.com>
2015-09-01 18:59:39 -0700

    Add user has_many votes test.

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :posts
+  has_many :votes
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -23,7 +23,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user has many votes" do
+    assert_equal [], users(:one).votes
+    Vote.create! user: users(:one), post: posts(:one)
+    assert_equal 1, users(:one).votes.count
+  end
 end
