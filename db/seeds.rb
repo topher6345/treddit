9e96838438d80406baec56dfb809fb5c04abb2b3
Christopher Saunders <topher6345@gmail.com>
2015-09-03 12:23:57 -0700

    Add development seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ActionMailer::Base.perform_deliveries = false
+
+require 'faker'
+
+
+PASSWORD = 'asdfjkl;'
+subtreddit_names = %w(Ruby Rails Music News Programming)
+
+subtreddits = []
+subtreddit_names.each do |name|
+  subtreddits << Subtreddit.create!(name: name, description: Faker::Commerce.department)
+end
+
+users = []
+
+User.create!(email: 'topher6345@gmail.com',
+                      password: PASSWORD,
+                      password_confirmation: PASSWORD).confirm
+
+20.times do
+  user = User.create!(email: Faker::Internet.email,
+                      password: PASSWORD,
+                      password_confirmation: PASSWORD)
+  user.confirm
+  users << user
+end
+
+posts = []
+20.times do
+  posts << Post.create!(title: Faker::Company.catch_phrase,
+                        body: Faker::Lorem.paragraph,
+                        user: users.sample,
+                        subtreddit: subtreddits.sample)
+end
+
+80.times do
+  posts << posts.sample.children.create!(body: Faker::Lorem.paragraph,
+                                  user: users.sample,
+                                  subtreddit: subtreddits.sample)
+end
+
+while(Vote.count < 40)
+  Upvote.create!(user: users.sample, post: posts.sample) rescue DuplicateUpvote
+end
+
+
+ActionMailer::Base.perform_deliveries = true
