fdbef5a3f4d97266f38fc6a5005707e69c0651cd
Christopher Saunders <topher6345@gmail.com>
2015-09-08 12:27:14 -0700

    Add bullet configuration.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,13 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+  end
 end
+
+
