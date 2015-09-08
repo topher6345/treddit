6304e4e5ebc5456f9746d76ebaa5adbadc8a53c0
Christopher Saunders <topher6345@gmail.com>
2015-09-08 13:31:36 -0700

    Update Comment test.

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,27 +2,6 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
 
-  test 'should create child post' do
-    assert_difference('Post.count') do
-      comment = Comment.new(
-        parent: posts(:one),
-        body: 'I like this post.',
-        user: users(:one))
-      comment.create_post
-    end
-  end
-
-  test 'should update parent cache' do
-    @post = posts(:one)
-    assert_equal 0, @post.descendants_depth
-    comment = Comment.new(
-        parent: posts(:one),
-        body: 'I like this post.',
-        user: users(:one))
-    comment.update_parent_cache
-    assert_equal 1, @post.descendants_depth
-  end
-
   test 'should create comment' do
     @post = posts(:one)
     assert_equal 0, @post.descendants_depth
@@ -32,6 +11,6 @@ class CommentTest < ActiveSupport::TestCase
         body: 'I like this post.',
         user: users(:one))
     end
-    assert_equal 1, @post.descendants_depth
+    assert_equal 1, Post.find(@post.id).descendants_depth
   end
 end
