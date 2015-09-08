d8221f91067cab2ff9d7919a3c476ff0d1b23206
Christopher Saunders <topher6345@gmail.com>
2015-09-08 14:54:00 -0700

    Add sort posts by comments.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
   # Displays all the root level Posts.
   # Serves as the 'Front Page' endpoint.
   def index
-    @posts = Post.where(ancestry_depth: 0).includes(:user, :subtreddit)
+    @posts = Post.where(ancestry_depth: 0).includes(:user, :subtreddit).order(descendants_depth: :desc)
   end
 
   # Displays a Post and its descendants
