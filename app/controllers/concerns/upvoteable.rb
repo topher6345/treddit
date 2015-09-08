61b1e12277521351ba622bf450a555fada331a89
Christopher Saunders <topher6345@gmail.com>
2015-09-07 18:48:07 -0700

    Add comments to Upvoteable module.

--- app/controllers/concerns/upvoteable.rb
+++ app/controllers/concerns/upvoteable.rb
@@ -1,11 +1,18 @@
+# = Upvoteable
+#
+# This module adds actions to upvote, or undo an existing upvote for a post.
+
 module Upvoteable
   extend ActiveSupport::Concern
+
+  # Defines an endpoint to add an upvote to an existing Post.
   def upvote
     post = Post.find(params[:id])
     Upvote.create! user: current_user, post: post
     render json: {status: :ok }, status: :ok
   end
 
+  # Defines an endpoint to undo an existing upvote to an existing Post.
   def destroy_upvote
     post = Post.find(params[:id])
     Upvote.destroy! user: current_user, post: post
