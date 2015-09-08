8831b9c4fc9383afbd029260153201c859030d04
Christopher Saunders <topher6345@gmail.com>
2015-09-07 18:47:45 -0700

    Add comments to Post model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,11 +14,39 @@
 #  votes          :integer          default(0)
 #  subtreddit_id  :integer          not null
 #
+#
+# = Post
+#
+# A post is the foundational record of Treddit.
+#
+# Posts can be viewed as nodes in a hierarchal tree data structure.
+#
+#   https://en.wikipedia.org/wiki/Tree_(data_structure)
+#
+# Posts can be roots (OP or original posts.)
+# or can be comments (Posts whos parent node is another Post.)
 
 class Post < ActiveRecord::Base
+
+  # Caches how many descendants (comments) a Post may have
+  #
+  #   https://github.com/stefankroes/ancestry#options-for-has_ancestry
+  #
   has_ancestry cache_depth: true
+
+  # Defines a Post (or comment) must have some content
   validates :body, presence: true
+
+  # Defines relationship between Post and User
   belongs_to :user
+
+  # Defines relationship between Post and Subtreddit, so that
+  # Posts can be viewed by Subtreddit.
+  #
+  #   https://en.wikipedia.org/wiki/Reddit#Subreddits
+  #
   belongs_to :subtreddit
+
+  # Convenience method that a Post can display what User wrote the post.
   delegate :email, to: :user
 end
