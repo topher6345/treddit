d6d8e015fcc3edc0c0b26acc9f8366ddd813f4d4
Christopher Saunders <topher6345@gmail.com>
2015-09-09 14:46:36 -0700

    Add CommentTest ActiveModel validations test cases.

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -13,4 +13,36 @@ class CommentTest < ActiveSupport::TestCase
     end
     assert_equal 1, Post.find(@post.id).descendants_depth
   end
+
+  test 'should raise ArgumentError if create is not passed attributes' do
+    assert_raises(ArgumentError) do
+      Comment.create!
+    end
+  end
+
+  test 'should not be valid if all attributes are empty' do
+    comment = Comment.new
+    assert_raises(ActiveModel::StrictValidationFailed) do
+      comment.valid?
+    end
+    comment.parent = posts(:one)
+    assert_raises(ActiveModel::StrictValidationFailed) do
+      comment.valid?
+    end
+    comment.body = 'I like this post.'
+    assert_raises(ActiveModel::StrictValidationFailed) do
+      comment.valid?
+    end
+    comment.user = users(:one)
+    assert comment.valid?
+  end
+
+  test 'should save and return ActiveRecord model if valid' do
+    comment = Comment.new(
+        parent: posts(:one),
+        body: 'I like this post.',
+        user: users(:one))
+
+    assert_not_nil comment.save!
+  end
 end
