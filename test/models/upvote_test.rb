071a8e9fc9191fb82178c8fcd7833f58ddcdffd5
Christopher Saunders <topher6345@gmail.com>
2015-09-26 23:28:21 -0700

    Refactor UpvoteTest.

--- test/models/upvote_test.rb
+++ test/models/upvote_test.rb
@@ -2,45 +2,52 @@ require 'test_helper'
 
 class UpvoteTest < ActiveSupport::TestCase
 
+  setup do
+    @user = users(:one)
+    @post = posts(:one)
+  end
+
   test 'upvote creates a vote' do
     assert_difference('Vote.count') do
-      Upvote.create!(user: users(:one), post: posts(:one))
+      Upvote.create!(user: @user, post: @post)
     end
   end
 
   test 'upvote increments a posts vote count' do
-    assert_difference('posts(:one).votes') do
-      Upvote.create!(user: users(:one), post: posts(:one))
+    assert_difference('@post.votes') do
+      Upvote.create!(user: @user, post: @post)
     end
   end
 
   test 'user cannot upvote the same post a second time' do
-    Upvote.create!(user: users(:one), post: posts(:one))
-    assert_raises(DuplicateUpvote) do
-      Upvote.create!(user: users(:one), post: posts(:one))
+    Upvote.create!(user: @user, post: @post)
+
+    assert_raises(Upvote::DuplicateUpvote) do
+      Upvote.create!(user: @user, post: @post)
     end
   end
 
   test 'upvote destroys a vote' do
-    Upvote.create!(user: users(:one), post: posts(:one))
+    Upvote.create!(user: @user, post: @post)
+
     assert_difference('Vote.count', -1) do
-      Upvote.destroy_all!(user: users(:one), post: posts(:one))
+      Upvote.destroy_all!(user: @user, post: @post)
     end
   end
 
-
   test 'user decrements a posts vote count' do
-    assert_difference('posts(:one).votes') do
-      Upvote.create!(user: users(:one), post: posts(:one))
+    assert_difference('@post.votes') do
+      Upvote.create!(user: @user, post: @post)
     end
-    assert_difference('posts(:one).votes',  -1) do
-      Upvote.destroy_all!(user: users(:one), post: posts(:one))
+
+    assert_difference('@post.votes', -1) do
+      Upvote.destroy_all!(user: @user, post: @post)
     end
   end
 
   test 'attempting to remove a nonexistent upvote throws exception' do
-    assert_raises(UpvoteNotFound) do
-      Upvote.destroy_all!(user: users(:one), post: posts(:one))
+    assert_raises(Upvote::UpvoteNotFound) do
+      Upvote.destroy_all!(user: @user, post: @post)
     end
   end
 end
