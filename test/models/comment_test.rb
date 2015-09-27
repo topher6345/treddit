ef107db5f4b032e3b45e9040814ed97280996b3a
Christopher Saunders <topher6345@gmail.com>
2015-09-26 23:17:15 -0700

    Update CommentTest.
    * put Fixture objects into setup method
    * get rid of exceptions in #valid? calls

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,14 +2,18 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
 
-  test 'should create comment' do
+  setup do
     @post = posts(:one)
+    @user = users(:one)
+  end
+
+  test 'should create comment' do
     assert_equal 0, @post.descendants_depth
     assert_difference('Post.count') do
       Comment.create!(
-        parent: posts(:one),
+        parent: @post,
         body: 'I like this post.',
-        user: users(:one))
+        user: @user)
     end
     assert_equal 1, Post.find(@post.id).descendants_depth
   end
@@ -22,26 +26,23 @@ class CommentTest < ActiveSupport::TestCase
 
   test 'should not be valid if all attributes are empty' do
     comment = Comment.new
-    assert_raises(ActiveModel::StrictValidationFailed) do
-      comment.valid?
-    end
-    comment.parent = posts(:one)
-    assert_raises(ActiveModel::StrictValidationFailed) do
-      comment.valid?
-    end
+    assert !comment.valid?
+
+    comment.parent = @post
+    assert !comment.valid?
+
     comment.body = 'I like this post.'
-    assert_raises(ActiveModel::StrictValidationFailed) do
-      comment.valid?
-    end
-    comment.user = users(:one)
+    assert !comment.valid?
+
+    comment.user = @user
     assert comment.valid?
   end
 
   test 'should save and return ActiveRecord model if valid' do
     comment = Comment.new(
-        parent: posts(:one),
-        body: 'I like this post.',
-        user: users(:one))
+      parent: @post,
+      body: 'I like this post.',
+      user: @user)
 
     assert_not_nil comment.save!
   end
