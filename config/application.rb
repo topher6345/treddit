ef1324670b2f7fadee7e85db2ac2a7925a9c7e4d
Christopher Saunders <topher6345@gmail.com>
2016-06-05 14:44:22 -0700

    Extract LOG_LEVEL ENV.

--- config/application.rb
+++ config/application.rb
@@ -38,7 +38,7 @@ module Treddit
     config.active_record.raise_in_transactional_callbacks = true
     config.cache_store = :redis_store, ENV.fetch('REDIS_URL')
     config.log_tags = [ :uuid ]
-    config.log_level = :debug
+    config.log_level = ENV['LOG_LEVEL']
     config.action_cable.mount_path = '/cable'
     config.action_cable.allowed_request_origins = ENV.fetch('ALLOWED_REQUEST_ORIGINS')
     config.action_cable.url = ENV.fetch('ACTION_CABLE_URL')
